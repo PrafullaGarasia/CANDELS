fab6725d7ec2a999abebb8c5bd03d8e9a6a90bf5
PrafullaGarasia <100746487+PrafullaGarasia@users.noreply.github.com>
2022-10-16 14:17:10 -0400

    Updated

---
+++ Migrations/20221016174325_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MvcCandel.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Candel",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type = table.Column<string>(nullable: true),
+                    Color = table.Column<string>(nullable: true),
+                    MadeFrom = table.Column<string>(nullable: true),
+                    Fragrance = table.Column<string>(nullable: true),
+                    ExpiryDate = table.Column<DateTime>(nullable: false),
+                    Price = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Candel", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Candel");
+        }
+    }
+}
---
+++ Models/SeedData.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcCandel.Data;
+
+namespace MvcCandel.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcCandelContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcCandelContext>>()))
+            {
+                // Look for any Candels.
+                if (context.Candel.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Candel.AddRange(
+                    new Candel
+                    {
+                        Type = "Natural",
+                        Color = "White",
+                        MadeFrom = "Bee Wax",
+                        Fragrance = "Yes",
+                        ExpiryDate = DateTime.Parse("2022-2-12"),
+                        Price = 7.99M
+                    },
+
+                    new Candel
+                    {
+                        Type = "Natural",
+                        Color = "Mix Color",
+                        MadeFrom = "Vegetable Wax",
+                        Fragrance = "Yes",
+                        ExpiryDate = DateTime.Parse("2022-5-13"),
+                        Price = 8.99M
+                    },
+
+                    new Candel
+                    {
+                        Type = "Natural",
+                        Color = "White",
+                        MadeFrom = "Soy",
+                        Fragrance = "No",
+                        ExpiryDate = DateTime.Parse("2022-6-14"),
+                        Price = 9.99M
+                    },
+
+                    new Candel
+                    {
+                        Type = "Artificial",
+                        Color = "Yellow",
+                        MadeFrom = "Plastic",
+                        Fragrance = "No",
+                        ExpiryDate = DateTime.Parse("2022-7-16"),
+                        Price = 3.99M
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
