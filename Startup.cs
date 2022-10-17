d3bc33c23380cf97c7be6722cc189d6a9ac588ca
PrafullaGarasia <100746487+PrafullaGarasia@users.noreply.github.com>
2022-10-17 11:30:54 -0400

    commit

--- Startup.cs
+++ Startup.cs
@@ -27,16 +27,12 @@ namespace MvcCandel
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddDbContext<ApplicationDbContext>(options =>
-            //    options.UseSqlServer(
-            //        Configuration.GetConnectionString("DefaultConnection")));
-            //services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-            //    .AddEntityFrameworkStores<ApplicationDbContext>();
-            //services.AddControllersWithViews();
-            services.AddControllersWithViews();
-
             services.AddDbContext<MvcCandelContext>(options =>
-            options.UseSqlServer(Configuration.GetConnectionString("MvcCandelContext")));
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("MvcCandelContext")));
+           services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+               .AddEntityFrameworkStores<MvcCandelContext>();
+            services.AddControllersWithViews();
             services.AddRazorPages();
         }
 
