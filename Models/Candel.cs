94dbd0746d89da250f578c9fd3a13c1eaf42c7e8
PrafullaGarasia <100746487+PrafullaGarasia@users.noreply.github.com>
2022-10-16 21:18:25 -0400

    Commit

--- Models/Candel.cs
+++ Models/Candel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcCandel.Models
 {
@@ -13,9 +14,10 @@ namespace MvcCandel.Models
         public string Color { get; set; }
         public string MadeFrom { get; set; }
         public string Fragrance { get; set; }
-
+        [Display(Name = "Expiry Date")]
         [DataType(DataType.Date)]
         public DateTime ExpiryDate { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
